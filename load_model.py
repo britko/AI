c9e27788c693cc2d0bcb08682d75873be3170290
britko <britko@naver.com>
2021-02-10 00:20:42 +0900

    Load_v2

--- load_model.py
+++ load_model.py
@@ -1,30 +1,19 @@
-import sys, os
 import tensorflow as tf
-import keras
-
-from PIL import Image
-import cv2
+import matplotlib.pyplot as plt
 import numpy as np
 from keras.models import load_model
 from tensorflow.keras.preprocessing import image
-from tensorflow.keras.preprocessing.image import ImageDataGenerator
 
-def imgG(img_file):
-    image = cv2.imread(img_file)
-    image = tf.image.resize(image, [64, 64])
-    image = tf.expand_dims(image, axis=0)   # the shape would be (1, 64, 64, 3)
+# 분류 가능하게 이미지 가공
+img1 = image.load_img(r'C:\Users\pc\Desktop\고영국\개발\AI\test_img\None_9.jpg', target_size=(64, 64))
+img = image.img_to_array(img1)
+img = img/255
+img = np.expand_dims(img, axis=0)
 
-print(imgG(r'C:\Users\pc\Desktop\고영국\개발\AI\Donut_10.jpg').shape)
+# print(img.shape)
 
+# 모델 불러오기
 model = load_model('checkpoint_v2-07-0.55-0.82.h5')
 
-print(model.predict_classes(image))
-
-model.summary()
-test_img = cv2.imread("Donut_10.jpg")
-print(test_img.dtype)
-test_img.imshow()
-test_img = np.reshape(test_img, ((0,) + test_img.shape))
-print(np.ndim(test_img))
-
-print('The Answer is ', model.predict_classes(test_img))
+# 분류!!!
+print('The Answer is ', model.predict_classes(img))
